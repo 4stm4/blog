1e3debd6964adc5b47d64cf05c79f022d8ce789f
aleksei-zakharchenko-tfs <99797280+aleksei-zakharchenko-tfs@users.noreply.github.com>
2022-07-25 19:45:22 +0300

    add markdown styles

---
+++ assets/js/clipboard_copy_component.js
@@ -0,0 +1,45 @@
+const CLIPBOARD_COPY_TIMER_DURATION = 2000;
+function showSVG(svg) {
+    svg.style.display = 'inline-block';
+}
+function hideSVG(svg) {
+    svg.style.display = 'none';
+}
+// Toggle a copy button.
+function showCopy(button) {
+    const [copyIcon, checkIcon] = button.querySelectorAll('.octicon');
+    if (!copyIcon || !checkIcon)
+        return;
+    showSVG(copyIcon);
+    hideSVG(checkIcon);
+}
+// Toggle a copy button.
+function showCheck(button) {
+    const [copyIcon, checkIcon] = button.querySelectorAll('.octicon');
+    if (!copyIcon || !checkIcon)
+        return;
+    hideSVG(copyIcon);
+    showSVG(checkIcon);
+}
+const clipboardCopyElementTimers = new WeakMap();
+console.log('start');
+document.addEventListener('clipboard-copy', function ({ target }) {
+    if (!(target instanceof HTMLElement))
+        return;
+    console.log(target)
+    // if (!target.hasAttribute('data-view-component'))
+    //     return;
+    console.log('Evet');
+    const currentTimeout = clipboardCopyElementTimers.get(target);
+    if (currentTimeout) {
+        clearTimeout(currentTimeout);
+        clipboardCopyElementTimers.delete(target);
+    }
+    else {
+        showCheck(target);
+    }
+    clipboardCopyElementTimers.set(target, setTimeout(() => {
+        showCopy(target);
+        clipboardCopyElementTimers.delete(target);
+    }, CLIPBOARD_COPY_TIMER_DURATION));
+});
