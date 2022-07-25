bf2e0b4c3851eab16634d97ff7aa501ec907324d
aleksei-zakharchenko-tfs <99797280+aleksei-zakharchenko-tfs@users.noreply.github.com>
2022-07-25 20:51:22 +0300

    fix clipboard icon

--- assets/js/code.js
+++ assets/js/code.js
@@ -18,7 +18,7 @@ var clipboard_copy = " \
         </svg> \
         <svg aria-hidden='true' height='16' viewBox='0 0 16 16' \
             version='1.1' width='16' data-view-component='true' \
-            class='octicon octicon-check js-clipboard-check-icon color-fg-success d-none m-2'> \
+            class='octicon octicon-check js-clipboard-check-icon color-fg-success m-2' style='display: none'> \
             <path fill-rule='evenodd' \
                 d='M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z'> \
             </path> \
